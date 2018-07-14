4c757d2927cc1cc156e6aafc9834086e8d69cd6b
Polaris1949 <Polaris1949@126.com>
2018-07-14 19:33:10 +0800

    Cleanup for account

---
+++ include/account/account.h
@@ -0,0 +1,41 @@
+#ifndef _ROCO_ACCOUNT_H
+#define _ROCO_ACCOUNT_H 1
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "accfile.h"
+
+namespace roco
+{
+
+class account;
+class user; // from "user.h"
+
+class account
+{
+    std::size_t _M_id;
+    std::string _M_name;
+    std::string _M_pwd;
+
+public:
+    account();
+
+    std::size_t id() const;
+    std::string name() const;
+    std::string pwd() const;
+
+    bool from_file(accfile& __f);
+    bool to_file(accfile& __f) const;
+    account& operator >> (accfile& __f);
+    account& operator << (accfile& __f);
+
+    friend std::ostream& operator << (std::ostream& __out, const account& __x);
+
+private:
+    friend class user;
+};
+
+}
+
+#endif
---
+++ src/account/account.cpp
@@ -0,0 +1,61 @@
+#include "account.h"
+
+namespace roco
+{
+
+account::account()
+    : _M_id(), _M_name(), _M_pwd()
+{}
+
+std::size_t account::id() const
+{
+    return this->_M_id;
+}
+
+std::string account::name() const
+{
+    return this->_M_name;
+}
+
+std::string account::pwd() const
+{
+    return this->_M_pwd;
+}
+
+bool account::from_file(accfile& __f)
+{
+    if (!__f)
+    {
+        this->_M_id = 0;
+        this->_M_name = "bad accfile";
+        return false;
+    }
+
+    this->_M_id = __f.id();
+    __f >> (this->_M_name) >> (this->_M_pwd);
+    return true;
+}
+
+bool account::to_file(accfile& __f) const
+{
+    __f.close();
+    __f.open(this->_M_id);
+    if (!__f) return false;
+
+    __f << (this->_M_name) << ' ' << (this->_M_pwd);
+    return true;
+}
+
+account& account::operator >> (accfile& __f)
+{
+    this->from_file(__f);
+    return *this;
+}
+
+account& account::operator << (accfile& __f)
+{
+    this->to_file(__f);
+    return *this;
+}
+
+}
