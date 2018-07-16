e5d315677f55e18bc6898bfff904b710b518c959
Polaris1949 <Polaris1949@126.com>
2018-07-16 20:11:04 +0800

    Update account support

--- src/account/account.cpp
+++ src/account/account.cpp
@@ -1,10 +1,22 @@
 #include "account.h"
+#include <utility>
 
 namespace roco
 {
 
 account::account()
     : _M_id(), _M_name(), _M_pwd()
+{}
+
+account::account(std::size_t __id)
+    : _M_id(__id), _M_name(), _M_pwd()
+{
+    this->from_file(std::move(accfile(__id)));
+}
+
+account::account(std::size_t __id, const std::string& __name, \
+    const std::string& __pwd)
+    : _M_id(__id), _M_name(__name), _M_pwd(__pwd)
 {}
 
 std::size_t account::id() const
@@ -22,7 +34,7 @@ std::string account::pwd() const
     return this->_M_pwd;
 }
 
-bool account::from_file(accfile& __f)
+bool account::from_file(accfile&& __f)
 {
     if (!__f)
     {
@@ -36,25 +48,25 @@ bool account::from_file(accfile& __f)
     return true;
 }
 
-bool account::to_file(accfile& __f) const
+bool account::to_file(accfile&& __f) const
 {
     __f.close();
-    __f.open(this->_M_id);
-    if (!__f) return false;
+    __f.open(this->_M_id, accfile::openmode::out);
+    if (!__f.is_open()) return false;
 
     __f << (this->_M_name) << ' ' << (this->_M_pwd);
     return true;
 }
 
-account& account::operator >> (accfile& __f)
+account& account::operator >> (accfile&& __f)
 {
-    this->from_file(__f);
+    this->from_file(std::move(__f));
     return *this;
 }
 
-account& account::operator << (accfile& __f)
+account& account::operator << (accfile&& __f)
 {
-    this->to_file(__f);
+    this->to_file(std::move(__f));
     return *this;
 }
 
