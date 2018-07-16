7a9e9582559a1a28a4dcff28b31eaffaccdf5b37
Polaris1949 <Polaris1949@126.com>
2018-07-16 20:12:30 +0800

    Remove some support to core

--- src/ui/ui.cpp
+++ src/ui/ui.cpp
@@ -1,52 +1,11 @@
 #include "ui.h"
 #include "account/user.h"
-#include <polaris/console>
+#include "console.h"
+#include "textbox.h"
 #include <cstdlib>
 
 namespace roco
 {
-
-char getch()
-{
-    char c;
-    system("stty -echo");
-    system("stty -icanon");
-    c=getchar();
-    system("stty icanon");
-    system("stty echo");
-    return c;
-}
-
-void getch_b()
-{
-    system("stty -echo");
-    system("stty -icanon");
-}
-
-char getch_f()
-{
-    return getchar();
-}
-
-void getch_e()
-{
-    system("stty icanon");
-    system("stty echo");
-}
-
-char getche()
-{
-    char c;
-    system("stty -icanon");
-    c=getchar();
-    system("stty icanon");
-    return c;
-}
-
-int sleep(double __s)
-{
-    return ::system((std::string("sleep ") + std::to_string(__s)).data());
-}
 
 void menu_fmt(const std::string& __s)
 {
@@ -69,36 +28,33 @@ int ui_main()
     menu_fmt("Exit");
     std::cout.flush();
     pol::console.hide_cursor();
-    char ch;
-    while (ch = getch())
-    {
-        if (ch == 'L' || ch == 'l')
-        {
-            ui_login();
-            break;
-        }
-        else if (ch == 'R' || ch == 'r')
-        {
-            ui_reg();
-            break;
-        }
-        else if (ch == 'H' || ch == 'h')
-        {
-            ui_help();
-            break;
-        }
-        else if (ch == 'E' || ch == 'e')
-        {
-            ui_exit();
-            break;
-        }
-        else
-        {
-            alert("Unknown option!");
-            continue;
-        }
-    }
+    char ch; bool e;
+    getch_b();
 
+    while (ch = getch_f())
+    {
+        switch (ch)
+        {
+            case 'L': case 'l': getch_e(); e = ui_login(); break;
+            case 'R': case 'r': getch_e(); ui_reg();  e = 0; break;
+            case 'H': case 'h': getch_e(); ui_help(); e = 0; break;
+            case 'E': case 'e': getch_e(); ui_exit(); e = 1; break;
+            default: alert_ex("Unknown option!");     e = 0; break;
+        }
+        if (e) break;
+        else getch_b();
+        pol::console.clear_screen();
+        pol::console.locate(0, 0);
+        std::cout << "Welcome to Roco World!\n\n";
+        std::cout << "Menu: \n";
+        menu_fmt("Login");
+        menu_fmt("Register");
+        menu_fmt("Help");
+        menu_fmt("Exit");
+        std::cout.flush();
+        pol::console.hide_cursor();
+    }
+    getch_e();
     pol::console.show_cursor();
     pol::console.purge();
     pol::console.clear_screen();
@@ -106,7 +62,7 @@ int ui_main()
     return 0;
 }
 
-void ui_login()
+bool ui_login()
 {
     pol::console.clear_screen();
 
@@ -133,118 +89,126 @@ void ui_login()
         std::cout << " to echo on/off the password.";
         pol::console.purge();
 
-        pol::console.locate(8, 1);
         unsigned id;
-        std::string pwd;
-        pol::console.show_cursor();
-        std::cin >> id;
-        pol::console.hide_cursor();
-        accfile af(id);
+
+        if (!numboxin_ex({1,8}, id))
+        {
+            alert_ex("Login cancelled!");
+            sleep(1);
+            return false;
+        }
+
         account a;
-        a.from_file(af);
+        a.from_file(std::move(accfile(id)));
 
         if (a.name().front() == '?')
         {
-            alert_clear();
-            alert("Bad userid!");
+            alert_ex("Bad userid!");
             std::cin.clear();
-            std::cin >> pwd;
             continue;
         }
 
         alert_clear();
-        pol::console.locate(10, 2);
-        pol::console.show_cursor();
-        getch_b();
-        char ch = getch_f();
-        bool echo = false;
-
-        while (ch = getch_f())
-        {
-            if (ch == 127)
-            {
-                if (!pwd.empty())
-                {
-                    pwd.pop_back();
-                    std::cout << '\b';
-                    std::cout.flush();
-                    pol::console.clear_line_after();
-                }
-                continue;
-            }
-            if (ch == 5)
-            {
-                if (echo)
-                {
-                    echo = false;
-                    pol::console.locate(10, 2);
-                    for (std::size_t i=0; i<pwd.size(); ++i)
-                        std::cout << '*';
-                }
-                else
-                {
-                    echo = true;
-                    pol::console.locate(10, 2);
-                    for (std::size_t i=0; i<pwd.size(); ++i)
-                        std::cout << pwd[i];
-                }
-                continue;
-            }
-            if (ch == '\n') break;
-            pwd += ch;
-            if (echo) std::cout << ch;
-            else std::cout << '*';
-            std::cout.flush();
-        }
-        getch_e();
-
-        pol::console.hide_cursor();
+        std::string pwd;
+        pwdboxin({2,10}, pwd);
 
         if (a.pwd() != pwd)
         {
-            alert_clear();
-            alert("Bad password!");
+            alert_ex("Bad password!");
             continue;
         }
 
         alert_clear();
         user u;
         u.login(a);
-        alert("Login succesfully!");
+        alert_ex("Login succesfully!");
         sleep(1);
-        break;
+        return true;
     }
 }
 
-void ui_reg()
-{}
+bool ui_reg()
+{
+    pol::console.clear_screen();
+
+    while (true)
+    {
+        pol::console.locate(36, 0);
+        pol::console.highlight();
+        std::cout << "Register";
+        pol::console.purge();
+        pol::console.locate(0, 1);
+        std::cout << "Userid: ";
+        pol::console.clear_line_after();
+        pol::console.locate(0, 2);
+        std::cout << "Username: ";
+        pol::console.clear_line_after();
+        pol::console.locate(0, 3);
+        std::cout << "Password: ";
+        pol::console.clear_line_after();
+
+        pol::console.locate(0, 4);
+        pol::console.foreground_color(1);
+        std::cout << "Note: You can use ";
+        pol::console.highlight();
+        std::cout << "Ctrl+E";
+        pol::console.purge();
+        pol::console.foreground_color(1);
+        std::cout << " to echo on/off the password.";
+        pol::console.purge();
+
+        unsigned id;
+
+        if (!numboxin_ex({1,8}, id))
+        {
+            alert_ex("Register cancelled!");
+            sleep(1);
+            return false;
+        }
+
+        if (!id)
+        {
+            alert_ex("Bad userid!");
+            std::cin.clear();
+            continue;
+        }
+
+        if (accfile(id).is_open())
+        {
+            alert_ex("Account existed!");
+            continue;
+        }
+
+        alert_clear();
+        std::string name;
+
+        if (!textboxin_ex({2,10}, name))
+        {
+            alert_ex("Register cancelled!");
+            sleep(1);
+            return false;
+        }
+
+        std::string pwd;
+        pwdboxin({3,10}, pwd);
+
+        user u;
+        if (!u.reg(account(id, name, pwd)))
+        {
+            alert("Register failed!");
+            continue;
+        }
+
+        alert("Register succesfully!");
+        sleep(1);
+        return true;
+    }
+}
 
 void ui_help()
 {}
 
 void ui_exit()
 {}
-
-void alert(const std::string& __s)
-{
-    pol::console.purge();
-    pol::console.save_cursor();
-    pol::console.locate(0, 23);
-    pol::console.highlight();
-    std::cout << __s;
-    std::cout.flush();
-    pol::console.recover_cursor();
-    pol::console.purge();
-}
-
-void alert_clear()
-{
-    pol::console.purge();
-    pol::console.save_cursor();
-    pol::console.locate(0, 23);
-    pol::console.clear_line_after();
-    pol::console.recover_cursor();
-    pol::console.purge();
-}
 
 }
