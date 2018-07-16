277e12241977da375aa2932cc7fb107c2bb6a6bb
Polaris1949 <Polaris1949@126.com>
2018-07-16 20:10:55 +0800

    Update ui

--- include/ui/ui.h
+++ include/ui/ui.h
@@ -2,24 +2,17 @@
 #define _ROCO_UI_H 1
 
 #include <string>
+#include "core.h"
 
 namespace roco
 {
 
-char getch();
-void getch_b();
-char getch_f();
-void getch_e();
-char getche();
-int sleep(double __s);
 void menu_fmt(const std::string& __s);
 int ui_main();
-void ui_login();
-void ui_reg();
+bool ui_login();
+bool ui_reg();
 void ui_help();
 void ui_exit();
-void alert(const std::string& __s);
-void alert_clear();
 
 }
 
