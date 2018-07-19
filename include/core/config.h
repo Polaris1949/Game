8878cdde2d729f64a1c75d0b14e9fab0509d35ea
Polaris1949 <Polaris1949@126.com>
2018-07-19 20:23:33 +0800

    Update config

--- include/core/config.h
+++ include/core/config.h
@@ -1,8 +1,10 @@
 #ifndef _ROCO_CONFIG_H
 #define _ROCO_CONFIG_H 1
 
-#define _ROCO_ROOTDIR "/cygdrive/d/develop/roco"
-#define _ROCO_VARDIR _ROCO_ROOTDIR "/var/roco"
+#define _ROCO_DIR_ROOT "/cygdrive/d/develop/roco"
+#define _ROCO_DIR_VAR _ROCO_DIR_ROOT "/var/roco"
+#define _ROCO_DIR_VAR_ACC _ROCO_DIR_VAR "/account"
+#define _ROCO_FE_ADB ".adb"
 
 namespace roco {}
 
